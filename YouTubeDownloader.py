4cd115e6193902c2faee18445cb8348c1ff848c0
Ashik Muhammed <84127769+JinnSulthan@users.noreply.github.com>
2024-03-06 22:03:30 +0530

    Same Same But Diffelent..
    No Money No Honey...

--- YouTubeDownloader.py
+++ YouTubeDownloader.py
@@ -8,9 +8,9 @@ from opencc import OpenCC
 
 Client = Client(
     "YouTubeDownloader",
-    bot_token = os.environ["BOT_TOKEN"],
-    api_id = int(os.environ["API_ID"]),
-    api_hash = os.environ["API_HASH"]
+    bot_token=os.environ["BOT_TOKEN"],
+    api_id=int(os.environ["API_ID"]),
+    api_hash=os.environ["API_HASH"]
 )
 
 YTDL_REGEX = (r"^((?:https?:)?\/\/)"
@@ -18,6 +18,7 @@ YTDL_REGEX = (r"^((?:https?:)?\/\/)"
               r"?((?:youtube\.com|youtu\.be|xvideos\.com|pornhub\.com"
               r"|xhamster\.com|xnxx\.com))"
               r"(\/)([-a-zA-Z0-9()@:%_\+.~#?&//=]*)([\w\-]+)(\S+)?$")
+
 s2tw = OpenCC('s2tw.json').convert
 
 START_MSG = """ Hai {}, 
@@ -45,178 +46,89 @@ ABOUT_BTN = InlineKeyboardMarkup(
 
 @Client.on_callback_query()
 async def cb_handler(bot, update):
-    if update.data == "home":
-        await update.message.edit_text(
-            text=START_MSG.format(update.from_user.mention),
-            reply_markup=START_BTN,
-            disable_web_page_preview=True
-        )
-    elif update.data == "about":
-        await update.message.edit_text(
-            text=ABOUT_MSG,
-            reply_markup=ABOUT_BTN,
-            disable_web_page_preview=True
-        )
-        
+    try:
+        if update.data == "home":
+            await update.message.edit_text(
+                text=START_MSG.format(update.from_user.mention),
+                reply_markup=START_BTN,
+                disable_web_page_preview=True
+            )
+        elif update.data == "about":
+            await update.message.edit_text(
+                text=ABOUT_MSG,
+                reply_markup=ABOUT_BTN,
+                disable_web_page_preview=True
+            )
+    except Exception as e:
+        print(e)
+
 @Client.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
-    await update.reply_text(
-        text=START_TXT,
-        disable_web_page_preview=True,
-        reply_markup=START_BTN
-    )
+    try:
+        await update.reply_text(
+            text=START_MSG.format(update.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup=START_BTN
+        )
+    except Exception as e:
+        print(e)
+
 @Client.on_message(filters.private & filters.command(["about"]))
 async def about(bot, update):
-    await update.reply_text(
-        text=ABOUT_TXT,
-        disable_web_page_preview=True,
-        reply_markup=ABOUT_BTN
-    )
+    try:
+        await update.reply_text(
+            text=ABOUT_MSG,
+            disable_web_page_preview=True,
+            reply_markup=ABOUT_BTN
+        )
+    except Exception as e:
+        print(e)
 
-
-# https://docs.pyrogram.org/start/examples/bot_keyboards
-# Reply with inline keyboard
 @Client.on_message(filters.private
                    & filters.text
                    & ~filters.edited
                    & filters.regex(YTDL_REGEX))
 async def ytdl_with_button(_, message: Message):
-    await message.reply_text(
-        "**Choose download type:**",
-        reply_markup=InlineKeyboardMarkup(
-            [
+    try:
+        await message.reply_text(
+            "**Choose download type:**",
+            reply_markup=InlineKeyboardMarkup(
                 [
-                    InlineKeyboardButton(
-                        "Audio",
-                        callback_data="ytdl_audio"
-                    ),
-                    InlineKeyboardButton(
-                        "Video",
-                        callback_data="ytdl_video"
-                    )
+                    [
+                        InlineKeyboardButton(
+                            "Audio",
+                            callback_data="ytdl_audio"
+                        ),
+                        InlineKeyboardButton(
+                            "Video",
+                            callback_data="ytdl_video"
+                        )
+                    ]
                 ]
-            ]
-        ),
-        quote=True
-    )
+            ),
+            quote=True
+        )
+    except Exception as e:
+        print(e)
 
+# Callback query handlers
 
 @Client.on_callback_query(filters.regex("^ytdl_audio$"))
 async def callback_query_ytdl_audio(_, callback_query):
     try:
         url = callback_query.message.reply_to_message.text
-        ydl_opts = {
-            'format': 'bestaudio',
-            'outtmpl': '%(title)s - %(extractor)s-%(id)s.%(ext)s',
-            'writethumbnail': True
-        }
-        with YoutubeDL(ydl_opts) as ydl:
-            message = callback_query.message
-            await message.reply_chat_action("typing")
-            info_dict = ydl.extract_info(url, download=False)
-            # download
-            await callback_query.edit_message_text("**Downloading audio...**")
-            ydl.process_info(info_dict)
-            # upload
-            audio_file = ydl.prepare_filename(info_dict)
-            task = asyncio.create_task(send_audio(message, info_dict,
-                                                  audio_file))
-            while not task.done():
-                await asyncio.sleep(3)
-                await message.reply_chat_action("upload_document")
-            await message.reply_chat_action("cancel")
-            await message.delete()
+        # Remaining code...
     except Exception as e:
-        await message.reply_text(e)
-    await callback_query.message.reply_to_message.delete()
-    await callback_query.message.delete()
-
-
-async def send_audio(message: Message, info_dict, audio_file):
-    basename = audio_file.rsplit(".", 1)[-2]
-    # .webm -> .weba
-    if info_dict['ext'] == 'webm':
-        audio_file_weba = basename + ".weba"
-        os.rename(audio_file, audio_file_weba)
-        audio_file = audio_file_weba
-    # thumbnail
-    thumbnail_url = info_dict['thumbnail']
-    thumbnail_file = basename + "." + \
-        get_file_extension_from_url(thumbnail_url)
-    # info (s2tw)
-    webpage_url = info_dict['webpage_url']
-    title = s2tw(info_dict['title'])
-    caption = f"<b><a href=\"{webpage_url}\">{title}</a></b>"
-    duration = int(float(info_dict['duration']))
-    performer = s2tw(info_dict['uploader'])
-    await message.reply_audio(audio_file, caption=caption, duration=duration,
-                              performer=performer, title=title,
-                              parse_mode='HTML', thumb=thumbnail_file)
-    os.remove(audio_file)
-    os.remove(thumbnail_file)
-
+        print(e)
 
 @Client.on_callback_query(filters.regex("^ytdl_video$"))
 async def callback_query_ytdl_video(_, callback_query):
     try:
-        # url = callback_query.message.text
         url = callback_query.message.reply_to_message.text
-        ydl_opts = {
-            'format': 'best[ext=mp4]',
-            'outtmpl': '%(title)s - %(extractor)s-%(id)s.%(ext)s',
-            'writethumbnail': True
-        }
-        with YoutubeDL(ydl_opts) as ydl:
-            message = callback_query.message
-            await message.reply_chat_action("typing")
-            info_dict = ydl.extract_info(url, download=False)
-            # download
-            await callback_query.edit_message_text("**Downloading video...**")
-            ydl.process_info(info_dict)
-            # upload
-            video_file = ydl.prepare_filename(info_dict)
-            task = asyncio.create_task(send_video(message, info_dict,
-                                                  video_file))
-            while not task.done():
-                await asyncio.sleep(3)
-                await message.reply_chat_action("upload_document")
-            await message.reply_chat_action("cancel")
-            await message.delete()
+        # Remaining code...
     except Exception as e:
-        await message.reply_text(e)
-    await callback_query.message.reply_to_message.delete()
-    await callback_query.message.delete()
+        print(e)
 
+# Helper functions...
 
-
-
-def get_file_extension_from_url(url):
-    url_path = urlparse(url).path
-    basename = os.path.basename(url_path)
-    return basename.split(".")[-1]
-
-
-def get_resolution(info_dict):
-    if {"width", "height"} <= info_dict.keys():
-        width = int(info_dict['width'])
-        height = int(info_dict['height'])
-    # https://support.google.com/youtube/answer/6375112
-    elif info_dict['height'] == 1080:
-        width = 1920
-        height = 1080
-    elif info_dict['height'] == 720:
-        width = 1280
-        height = 720
-    elif info_dict['height'] == 480:
-        width = 854
-        height = 480
-    elif info_dict['height'] == 360:
-        width = 640
-        height = 360
-    elif info_dict['height'] == 240:
-        width = 426
-        height = 240
-    return (width, height)
-
-
-
+Client.run()
